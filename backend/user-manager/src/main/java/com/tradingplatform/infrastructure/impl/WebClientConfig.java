5d5cccbf6e3d9ed07d5a3cae4053904b95d92359
AngelG25 <angel.garcia.cortes25@gmail.com>
2026-06-06 18:02:29 +0200

    fix web client to use 1.1 HTTP

--- backend/user-manager/src/main/java/com/tradingplatform/infrastructure/impl/WebClientConfig.java
+++ backend/user-manager/src/main/java/com/tradingplatform/infrastructure/impl/WebClientConfig.java
@@ -2,13 +2,22 @@ package com.tradingplatform.infrastructure.impl;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
+
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        HttpClient httpClient = HttpClient.create()
+                .protocol(HttpProtocol.HTTP11);
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
